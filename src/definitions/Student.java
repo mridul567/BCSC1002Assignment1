b37dfccb332750c34e5c641692c12387ceafcad0
mridul61 <mridul.bhardwaj_cs19@gla.ac.in>
2020-08-27 20:50:31 +0530

    add private fields to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,10 +1,16 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
+ *  User: Mridul Bhardwaj (mridul567)
+ *  Date: 22/08/20
  *  Time: 3:49 PM
  *  File Name : Student.java
  * */
 package definitions;
 
 public class Student {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private long universityRollNumber;
+    private int numberOfBooksIssuedByStudent;
+    private Book[] nameOfAllBooksIssuedByStudent;
 }
