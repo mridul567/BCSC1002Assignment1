c30598ab00f0437888be8fb83373170f1735c5fa
mridul61 <mridul.bhardwaj_cs19@gla.ac.in>
2020-08-28 21:34:38 +0530

    add getters and setters for private fields to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -13,4 +13,52 @@ public class Student {
     private long universityRollNumber;
     private int numberOfBooksIssuedByStudent;
     private Book[] nameOfAllBooksIssuedByStudent;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+    public int getNumberOfBooksIssuedByStudent() {
+        return numberOfBooksIssuedByStudent;
+    }
+
+    public void setNumberOfBooksIssuedByStudent(int numberOfBooksIssuedByStudent) {
+        this.numberOfBooksIssuedByStudent = numberOfBooksIssuedByStudent;
+    }
+
+    public Book[] getNameOfAllBooksIssuedByStudent() {
+        return nameOfAllBooksIssuedByStudent;
+    }
+
+    public void setNameOfAllBooksIssuedByStudent(Book[] nameOfAllBooksIssuedByStudent) {
+        this.nameOfAllBooksIssuedByStudent = nameOfAllBooksIssuedByStudent;
+    }
 }
