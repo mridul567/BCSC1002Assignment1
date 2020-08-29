d845bcc1a1c16b5176b25894edc0624fff6b5d04
mridul61 <mridul.bhardwaj_cs19@gla.ac.in>
2020-08-29 15:42:33 +0530

    add constants, switch statement to FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,10 +6,53 @@
  * */
 package execution;
 
-import definitions.Student;
+import definitions.Library;
+
+import java.util.Scanner;
 
 public class FrontDesk {
-    public static void main(String[] args){
 
-    }
+        private static final int ISSUE_NEW_BOOK = 1;
+        private static final int RETURN_ISSUED_BOOK = 2;
+        private static final int SHOW_ALL_ISSUED_BOOKS = 3;
+        private static final int EXIT = 4;
+
+        public static void main(String [] args) {
+            Scanner scanner =  new Scanner(System.in);
+            int studentInput;
+            Library library = new Library();
+            do {
+                System.out.println("_=_=__=_=_ 'Welcome To The Front Desk'");
+                System.out.println("How may I help You today?");
+                System.out.println("1. Issue a new book for me.");
+                System.out.println("2. Return a previously issues book.");
+                System.out.println("3. Show me all my issued books.");
+                System.out.println("4. Exit.");
+                System.out.println("Enter your choice (1..4): ");
+                studentInput = scanner.nextInt();
+                switch (studentInput) {
+                    case ISSUE_NEW_BOOK:
+                        System.out.println("Enter the name of the book you want to issue: ");
+                        scanner.nextLine();
+                        String bookName = scanner.nextLine();
+                        library.issueBook(bookName);
+                        break;
+                    case RETURN_ISSUED_BOOK:
+                        System.out.println("Enter the name of the book which you want to return: ");
+                        scanner.nextLine();
+                        bookName = scanner.nextLine();
+                        library.returnBook(bookName);
+                        break;
+                    case SHOW_ALL_ISSUED_BOOKS:
+                        library.showAllIssuedBooks();
+                        break;
+                    case EXIT:
+                        System.out.println("Thanks for using Library Book Inventory System.");
+                        break;
+                    default:
+                        System.out.println("WRONG CHOICE");
+                }
+            }while (studentInput != EXIT);
+            scanner.close();
+        }
 }
